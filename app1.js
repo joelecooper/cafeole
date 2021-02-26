5100cf19e1f9d0b8b718dd3473712645eb92a0e1
Joel Cooper <joel.e.cooper>
2021-02-26 12:25:58 -0600

    added javascript files

--- app1.js
+++ app1.js
@@ -1,3 +1,11 @@
-function greetUser{
-    
+'use strict'
+
+let userName = prompt('Please Enter your Name: ');
+
+document.write('<h3>' + userName + '<h3>')
+
+if (userName == 'Joel'){
+    document.write('<h3> A true connoisseur of coffee has blessed this page and his name is Joel');
+} else {
+    document.write('<h3>' + 'Hey! Welcome to Cafeole,' + userName + '<h3>');
 }
